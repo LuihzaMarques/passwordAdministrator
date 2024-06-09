71d3eeb606cf7fe3dfa6ab8cbcd2087142a60974
Marco Antonio <dev.mmarco@gmail.com>
2024-06-09 10:20:22 -0400

    feat: novas funções

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,8 +1,12 @@
 from config.db import conn
-from schemas.user import usersEntity
+from schemas.user import users_list, users_password_description
 
 def list_all_users():
-  return usersEntity(conn.local.user.find())
+  return users_list(conn.local.user.find())
+
+def list_all_user_passwords():
+  count_password = conn.local.user.count_documents({})
+  return {"total_passwords": count_password}
 
 def database_user_verification(username, password):
   for x in conn.local.user.find({}, {"_id": 0, "name": 0}):
