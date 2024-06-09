c4ecd9a184ad9b296c367337063c12d4722597ae
Marco Antonio <dev.mmarco@gmail.com>
2024-06-09 10:19:15 -0400

    fix: formatação do código

--- utils/server_utils.py
+++ utils/server_utils.py
@@ -1,15 +1,9 @@
 from config.db import conn
+from utils.db_utils import database_user_verification
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 security = HTTPBasic()
-
-def database_user_verification(username, password):
-  for x in conn.local.user.find({}, {"_id": 0, "name": 0}):
-    if username == x["user"] and password == x["password"]:
-      return True
-
-  return False
 
 def authenticate_user(credentials:HTTPBasicCredentials = Depends(security)):
   if not database_user_verification(credentials.username, credentials.password):
