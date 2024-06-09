4e93ba7e3208c68f9b5dfcd853a2aec3e9f279d8
Marco Antonio <dev.mmarco@gmail.com>
2024-06-09 10:24:08 -0400

    feat: novos esquemas

--- schemas/user.py
+++ schemas/user.py
@@ -1,4 +1,4 @@
-def userEntity(item) -> dict:
+def user_entity(item) -> dict:
     return {
         "id": str(item["_id"]),
         "name": item["name"],
@@ -6,17 +6,20 @@ def userEntity(item) -> dict:
         "password": item["password"]
     }
 
+def users_entity(entity) -> list:
+    return[user_entity(item) for item in entity]
 
-def usersEntity(entity) -> list:
-      return[userEntity(item) for item in entity]
+def user_list(item) -> dict:
+    return {"user": item["user"]}
 
+def users_list(entity) -> list:
+    return[user_list(item) for item in entity]
 
-def userPasswordDescription(item: dict) -> dict:
+def user_password_description(item: dict) -> dict:
     return {
       #  "description" : item["description"],
         "password": item["password"]
     }
 
-
-def usersPasswordDescription(entity) -> list:
-      return[userPasswordDescription(item) for item in entity]
+def users_password_description(entity) -> list:
+    return[user_password_description(item) for item in entity]
