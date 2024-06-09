4fe961149df69c17f6aaaf541087f6118b910cdb
Marco Antonio <dev.mmarco@gmail.com>
2024-06-09 10:39:23 -0400

    feat: função de criar usuário no banco

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,5 +1,5 @@
 from config.db import conn
-from schemas.user import users_list, users_password_description
+from schemas.user import users_list
 
 def list_all_users():
   return users_list(conn.local.user.find())
@@ -7,6 +7,21 @@ def list_all_users():
 def list_all_user_passwords():
   count_password = conn.local.user.count_documents({})
   return {"total_passwords": count_password}
+
+def create_user_db(user, password):
+  new_user = dict(user)
+
+  if "id" in new_user:
+    del new_user["id"]
+
+  new_user["password"] = password
+  id = conn.local.user.insert_one(new_user).inserted_id
+  user_from_db = conn.local.user.find_one({"_id": id})
+
+  user_from_db["password"] = password
+  user_from_db["id"] = str(id)
+
+  return user_from_db
 
 def database_user_verification(username, password):
   for x in conn.local.user.find({}, {"_id": 0, "name": 0}):
